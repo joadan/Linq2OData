e0e1b59e4267fc456594a2343a840e2ed8e4f955
Joakim Dangården <Joakim.Dangarden@swegon.com>
2026-01-28 12:46:18 +0100

    Upaded Clients

--- test/Linq2OData.DemoClient/DemoClientV4/Types/Employee.cs
+++ test/Linq2OData.DemoClient/DemoClientV4/Types/Employee.cs
@@ -7,7 +7,7 @@ using Linq2OData.Client;
 
 namespace DemoClientV4.ODataDemo;
 
-public partial class Employee : Person
+public partial class Employee : ODataDemo.Person
 {
 
     [ODataMember("EmployeeID")]
--- test/Linq2OData.DemoClient/DemoClientV4/Types/FeaturedProduct.cs
+++ test/Linq2OData.DemoClient/DemoClientV4/Types/FeaturedProduct.cs
@@ -7,7 +7,7 @@ using Linq2OData.Client;
 
 namespace DemoClientV4.ODataDemo;
 
-public partial class FeaturedProduct : Product
+public partial class FeaturedProduct : ODataDemo.Product
 {
 
 
