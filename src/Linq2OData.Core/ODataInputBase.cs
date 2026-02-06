2731f6e6c4a4a8faf78cb12a3d1d781fce55b7a3
Joakim Dangården <Joakim.Dangarden@swegon.com>
2026-02-06 13:33:17 +0100

    Fixed Input Serialization

--- src/Linq2OData.Core/ODataInputBase.cs
+++ src/Linq2OData.Core/ODataInputBase.cs
@@ -17,12 +17,6 @@ namespace Linq2OData.Core
             return Values.ToDictionary(e => e.Key, e => e.Value);
         }
 
-        internal string GetValuesJson()
-        {
-            
-            return JsonSerializer.Serialize(GetValues());
-        }
-
 
         protected void ClearValues()
         {
@@ -43,10 +37,10 @@ namespace Linq2OData.Core
         protected void SetValue(string name, object? value)
         {
             Values.TryRemove(name, out _);
-            if (value != null)
-            {
-                Values.TryAdd(name, value);
-            }
+            // Always add the value, even if null
+            // This allows explicitly setting a property to null (important for UPDATE operations)
+            // Properties that are never set won't be in the dictionary at all
+            Values.TryAdd(name, value!);
         }
     }
 
