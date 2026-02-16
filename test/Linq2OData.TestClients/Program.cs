629a785d788ea51d381b586b28b8600e5bb94d2d
Joakim Dangården <Joakim.Dangarden@swegon.com>
2026-02-16 12:51:40 +0100

    Added TripPin sampleservice

--- test/Linq2OData.TestClients/Program.cs
+++ test/Linq2OData.TestClients/Program.cs
@@ -9,24 +9,41 @@ namespace Linq2OData.TestClients
 
         const string demoUrlV2 = "https://services.odata.org/V2/(S(jo0zj0zu5nmnrfcfj2zv1ny2))/OData/OData.svc/";
         const string demoUrlV4 = "https://services.odata.org/V4/(S(jo0zj0zu5nmnrfcfj2zv1ny2))/OData/OData.svc/";
+        const string tripPinUrl = "https://services.odata.org/V4/(S(0qbgsoxblavd1okt2uawjtfe))/TripPinServiceRW/";
 
         static async Task Main(string[] args)
         {
             Console.WriteLine("Here we go!");
 
-           // await GenerateDemoClientV2Async();
-             //await GenerateDemoClientV4Async();
+            // await GenerateDemoClientV2Async();
+            //   await GenerateDemoClientV4Async();
+           //await GenerateTripPinClientAsync();
 
-            await TestV2ClientAsync();
-           // await TestV4ClientAsync();
+            await TestTripPinAsync();
+            // await TestV4ClientAsync();
             //TestAddHocClient();
 
 
         }
 
-        private static void TestAddHocClient()
+        private static async Task TestTripPinAsync()
         {
-            var odataClient = new Linq2OData.Core.ODataClient(new HttpClient(), Core.ODataVersion.V2);
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(tripPinUrl)
+            };
+
+            var tripPinClient = new TripPin.TripPinClient(httpClient);
+
+            var result = await tripPinClient
+                .Query<TripPin.Microsoft.OData.SampleService.Models.TripPin.Person>()
+                .Top(1)
+                .Expand(e => e.Trips!.Select(e => e.PlanItems))
+         
+                .ExecuteAsync();
+
+            var t = result;
+
 
         }
 
@@ -113,6 +130,26 @@ namespace Linq2OData.TestClients
             var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
             if (projectDirectory == null) { throw new Exception("Unable to get project directory"); }
             var files = generator.GenerateClient(Path.Combine(projectDirectory, "DemoClientV4"));
+        }
+
+        private static async Task GenerateTripPinClientAsync()
+        {
+            var httpClient = new HttpClient();
+            var metadata = await httpClient.GetStringAsync(tripPinUrl + "$metadata");
+
+            var request = new Linq2OData.Generator.Models.ClientRequest
+            {
+                Name = "TripPinClient",
+                Namespace = "TripPin",
+            };
+            request.AddMetadata(metadata);
+
+            var generator = new Linq2OData.Generator.ClientGenerator(request);
+
+
+            var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+            if (projectDirectory == null) { throw new Exception("Unable to get project directory"); }
+            var files = generator.GenerateClient(Path.Combine(projectDirectory, "TripPinClientV4"));
         }
 
     }
